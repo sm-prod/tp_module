749307c8af518a6d3eb727bfc9333d45d42b2b31
totsamiyya <panck1@ya.ru>
2017-08-15 15:36:53 +0300

    Первый релиз

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,6 +8,8 @@
 
 QSerialPort serial;
 
+uint8_t adc_i,t_i,t_u_i,adc_u_i;
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -21,6 +23,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->pushButton_7->setToolTip("платнородий-платиновая термопара");
     ui->pushButton_5->setToolTip("платнородий-платиновая термопара");
     ui->pushButton_8->setToolTip("медь-константановая термопара");
+    ui->label->setToolTip("Установлен тип A(def)");
+    ui->lineEdit->setPlaceholderText("Команда");
 
     connect(&serial, &QSerialPort::readyRead, this, &MainWindow::readSerial);
     //read avaible comports
@@ -51,13 +55,21 @@ void MainWindow::on_pushButton_9_clicked()
     serial.setStopBits(QSerialPort::OneStop);
     serial.setFlowControl(QSerialPort::NoFlowControl);
     //connect(&serial, &QSerialPort::readyRead, this, &MainWindow::readSerial);
-    serial.open(QSerialPort::ReadWrite);
+    if (!serial.open(QSerialPort::ReadWrite))
+    {
+        writeData("Порт не удалось открыть");
+    }
+    else
+    {
+        writeData("Порт открыт");
+    }
 }
 
 void MainWindow::on_pushButton_10_clicked()
 {
     //close
     serial.close();
+    writeData("Порт закрыт");
 }
 
 
@@ -84,16 +96,19 @@ void MainWindow::readSerial()
 void MainWindow::on_pushButton_clicked()
 {
     serial.write("*set_t_A$");
+    ui->label->setToolTip("Установлен тип A");
 }
 
 void MainWindow::on_pushButton_2_clicked()
 {
     serial.write("*set_t_B$");
+    ui->label->setToolTip("Установлен тип B");
 }
 
 void MainWindow::on_pushButton_3_clicked()
 {
     serial.write("*set_t_J$");
+    ui->label->setToolTip("Установлен тип J");
 }
 
 
@@ -108,24 +123,128 @@ void MainWindow::writeData(QByteArray data)
 void MainWindow::on_pushButton_4_clicked()
 {
     serial.write("*set_t_K$");
+    ui->label->setToolTip("Установлен тип K");
 }
 
 void MainWindow::on_pushButton_8_clicked()
 {
     serial.write("*set_t_T$");
+    ui->label->setToolTip("Установлен тип T");
 }
 
 void MainWindow::on_pushButton_6_clicked()
 {
     serial.write("*set_t_N$");
+    ui->label->setToolTip("Установлен тип N");
 }
 
 void MainWindow::on_pushButton_7_clicked()
 {
     serial.write("*set_t_R");
+    ui->label->setToolTip("Установлен тип R");
 }
 
 void MainWindow::on_pushButton_5_clicked()
 {
     serial.write("*set_t_S$");
+    ui->label->setToolTip("Установлен тип S");
+}
+
+void MainWindow::on_pushButton_11_clicked()
+{
+    switch (adc_i)
+    {
+    case 0:
+    {
+        serial.write("*ADC_ON$");
+        adc_i=1;
+        ui->pushButton_11->setText("Отображение ADCW\nВключено");
+        break;
+    }
+    case 1:
+    {
+        serial.write("*ADC_OFF$");
+        adc_i=0;
+        ui->pushButton_11->setText("Отображение ADCW\nВыключено");
+        break;
+    }
+    default:
+        break;
+    }
+}
+
+void MainWindow::on_pushButton_12_clicked()
+{
+    switch (t_i)
+    {
+    case 0:
+    {
+        serial.write("*t_i_ON$");
+        t_i=1;
+        ui->pushButton_12->setText("Отображение t°C ХС\nВключено");
+        break;
+    }
+    case 1:
+    {
+        serial.write("*t_i_OFF$");
+        t_i=0;
+        ui->pushButton_12->setText("Отображение t°C ХС\nВыключено");
+        break;
+    }
+    default:
+        break;
+    }
+}
+
+void MainWindow::on_pushButton_13_clicked()
+{
+    switch (t_u_i)
+    {
+    case 0:
+    {
+        serial.write("*t_u_i_ON$");
+        t_u_i=1;
+        ui->pushButton_13->setText("Отображение U(t°C) ХС\nВключено");
+        break;
+    }
+    case 1:
+    {
+        serial.write("*t_u_i_OFF$");
+        t_u_i=0;
+        ui->pushButton_13->setText("Отображение U(t°C) ХС\nВыключено");
+        break;
+    }
+    default:
+        break;
+    }
+}
+
+void MainWindow::on_pushButton_14_clicked()
+{
+    switch (adc_u_i)
+    {
+    case 0:
+    {
+        serial.write("*u_i_ON$");
+        adc_u_i=1;
+        ui->pushButton_14->setText("Отображение U\nВключено");
+        break;
+    }
+    case 1:
+    {
+        serial.write("*u_i_OFF$");
+        adc_u_i=0;
+        ui->pushButton_14->setText("Отображение U\nВыключено");
+        break;
+    }
+    default:
+        break;
+    }
+}
+
+void MainWindow::on_pushButton_15_clicked()
+{
+    QString input = ui->lineEdit->text();
+    //char send[16];
+    serial.write(qPrintable(input));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -41,6 +41,16 @@ private slots:
 
     void on_pushButton_5_clicked();
 
+    void on_pushButton_11_clicked();
+
+    void on_pushButton_12_clicked();
+
+    void on_pushButton_13_clicked();
+
+    void on_pushButton_14_clicked();
+
+    void on_pushButton_15_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
